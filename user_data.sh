c4b2fae8b7df37f015500db1e9faca1a0446934a
alizaidansp <ali.zaidan@amalitech.com>
2025-03-20 09:02:16 +0000

    modified used_data

--- user_data.sh
+++ user_data.sh
@@ -135,8 +135,7 @@ server {
 
       # SvelteKit SSR handler
     location / {
-        proxy_pass http://127.0.0.1:3000;  # Ensure your SvelteKit app >
-        proxy_set_header Host \$host;
+        proxy_pass http://127.0.0.1:3000;  
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
